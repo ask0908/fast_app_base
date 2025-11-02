72520e395afd74564071447e87319d56e1b7c4e8
ask0908 <askdfjaslk@naver.com>
2025-11-02 14:16:57 +0900

    빌드 에러 수정, 스플래시 아이콘 추가

--- lib/common/util/app_keyboard_util.dart
+++ lib/common/util/app_keyboard_util.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/cupertino.dart';
-import 'package:keyboard_utils_fork/keyboard_listener.dart' as k;
-import 'package:keyboard_utils_fork/keyboard_utils.dart';
+import 'package:flutter/widgets.dart';
 
 class AppKeyboardUtil {
   static void hide(BuildContext context) {
@@ -19,17 +17,15 @@ class AppKeyboardUtil {
 }
 
 mixin KeyboardDetector<T extends StatefulWidget> on State<T> {
-  final keyboardUtils = KeyboardUtils();
-  int? subscribingId;
   bool isKeyboardOn = false;
   final bool useDefaultKeyboardDetectorInit = true;
 
   @override
   void initState() {
+    super.initState();
     if (useDefaultKeyboardDetectorInit) {
       initKeyboardDetector();
     }
-    super.initState();
   }
 
   @override
@@ -38,27 +34,42 @@ mixin KeyboardDetector<T extends StatefulWidget> on State<T> {
     super.dispose();
   }
 
-  initKeyboardDetector(
-      {final Function(double)? willShowKeyboard, final Function()? willHideKeyboard}) {
-    subscribingId = keyboardUtils.add(
-        listener: k.KeyboardListener(willHideKeyboard: () {
+  void initKeyboardDetector({
+    final Function(double)? willShowKeyboard,
+    final Function()? willHideKeyboard,
+  }) {
+    // WidgetsBinding을 사용한 키보드 감지
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _checkKeyboard(willShowKeyboard, willHideKeyboard);
+    });
+  }
+
+  void _checkKeyboard(
+      Function(double)? willShowKeyboard,
+      Function()? willHideKeyboard,
+      ) {
+    final mediaQuery = MediaQuery.of(context);
+    final viewInsets = mediaQuery.viewInsets;
+    final keyboardHeight = viewInsets.bottom;
+
+    if (keyboardHeight > 0 && !isKeyboardOn) {
+      // 키보드가 올라옴
+      isKeyboardOn = true;
+      if (willShowKeyboard != null) {
+        willShowKeyboard(keyboardHeight);
+      }
+      if (mounted) setState(() {});
+    } else if (keyboardHeight == 0 && isKeyboardOn) {
+      // 키보드가 내려감
+      isKeyboardOn = false;
       if (willHideKeyboard != null) {
         willHideKeyboard();
       }
-      setState(() {
-        isKeyboardOn = false;
-      });
-    }, willShowKeyboard: (value) {
-      if (willShowKeyboard != null) {
-        willShowKeyboard(value);
-      }
-      setState(() {
-        isKeyboardOn = true;
-      });
-    }));
+      if (mounted) setState(() {});
+    }
   }
 
-  disposeKeyboardDetector() {
-    keyboardUtils.unsubscribeListener(subscribingId: subscribingId);
+  void disposeKeyboardDetector() {
+    // 필요한 정리 작업
   }
 }
